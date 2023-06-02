44c739ea3f1da36254ca5fb9e46a55487924d8b9
Martbul <99181339+Martbul@users.noreply.github.com>
2023-06-03 00:34:51 +0300

    1
    2

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
 function validate() {
-    const emailInput = document.getElementById('email')
-    emailInput.addEventListener('change',onChange )
-
-    function onChange(e) {
-        const {target} = e
-        const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
-
-        if(pattern.test(target.value)) {
-            target.classList.remove('error')
-        }else{
-            target.classList.add('error')
-        }
-    }
+    console.log('TODO:...');
 }
