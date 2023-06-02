0b44d38f7b9af2ba2fa23ba30cd1a5d60fdd8529
Martbul <99181339+Martbul@users.noreply.github.com>
2023-06-03 00:39:54 +0300

    3
    3

--- solution.js
+++ solution.js
@@ -1,4 +1,30 @@
 function solve() {
+  let CAMEL_CASE = 'Camel Case';
+  let PASCAL_CASE = 'Pascal Case';
+  let textInput = document.getElementById("text").value.toLowerCase();
+  let currentText = document.getElementById("naming-convention").value;
+ const result =  document.getElementById("result");
 
-    //TODO...
+ if(currentText !== CAMEL_CASE && currentText !== PASCAL_CASE){
+result.textContent = "Error"
+  return;
+
+ } 
+
+
+ let arrOfStr = textInput.split(" ");
+let outputPoint ="";
+let startingPoint = 0;
+if(currentText === CAMEL_CASE){
+  outputPoint += arrOfStr[0]
+  startingPoint = 1;
+}
+
+
+for(let i = startingPoint; i < arrOfStr.length; i++){
+  outputPoint += arrOfStr[i][0].toUpperCase() + arrOfStr[i].slice(1, arrOfStr.length);
+
+
+  result.textContent = outputPoint
+}
 }
