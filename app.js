7de608c7a6c77a5ce2b9c5da1290d3abc0d38354
Martbul <99181339+Martbul@users.noreply.github.com>
2023-06-05 22:56:36 +0300

    342
    4343

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
-function lockedProfile() {
-   const SHOW_MORE = 'Show more';
-   const HIDE_IT = 'Hide it';
+function attachGradientEvents() {
+    let gradient = document.getElementById('gradient');
+    gradient.addEventListener('mousemove', hoverHandler)
+    let result = document.getElementById("result")
 
-   const buttonElements = Array.from(document.querySelectorAll('div button'))
-
-   for (const btn of buttonElements) {
-    btn.addEventListener('click',show)
-    
-   }
-
-   function show(e){
-    const divChildren = Array.from(e.target.parentElement.children);
-    const isLocked = divChildren[2].checked;
-
-
-    if(isLocked){
-        return;
-    }else{
-
-        const hiddenFieldElements= e.target.previousElementSibling;
-
-
-        if(e.target.textContent === SHOW_MORE){
-            hiddenFieldElements.style.display = 'inline';
-            e.target.textContent = HIDE_IT
-            return	;
-
-        }else{
-
-
-             hiddenFieldElements.style.display = '';
-             e.target.textContent = SHOW_MORE
-        }
+    function hoverHandler(e){
+        let offsetX = e.offsetX;
+        let element = e.target;
+        let elementX = element.clientWidth;
+        let percent = Math.trunc((offsetX / elementX) * 100);
+        result.textContent = `${percent}%`;
     }
-    }
-   
+
+
+
+
+
+ 
 }
