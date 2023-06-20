b2342c4d3884a0ec41537936b1a066315fbce29f
Martbul <99181339+Martbul@users.noreply.github.com>
2023-06-20 20:56:56 +0300

    TEST wine problem

--- test.js
+++ test.js
@@ -1,34 +1,95 @@
-class InafeBox {
-    _domElement;
-    constructor(parent , imageSources) {
-        this._domElement = this.#createElement(parent, imageSources);
-    }
+const chai = require('chai');	
+const {expect, assert} = chai
 
-    showHideImages() {
-        let image = this._domElement.querySelector('img')
-        let isVisible = image.style.display === 'block'
-        image.style.display =isVisible ? 'none' : 'block';
-        let button = this._domElement.querySelector('button')
-        button.textContent = isVisible ? 'Hide image' :"Show image"
-    }
+const findNewApartment = {
+    isGoodLocation(city, nearPublicTransportation) {
+      if (typeof city !== "string" || typeof nearPublicTransportation !== "boolean"){
+          throw new Error("Invalid input!");
+      }
+      if (city !== "Sofia" && city !== "Plovdiv" && city !== "Varna") {
+          return "This location is not suitable for you.";
+      }else {
+          if (nearPublicTransportation == true) {
+              return "You can go on home tour!";
+          }
+          else {
+              return "There is no public transport in area.";
+          }
+      }
+    },
+    isLargeEnough(apartments, minimalSquareMeters) {
+      let resultArr = [];
+      if (!Array.isArray(apartments) || typeof minimalSquareMeters !== "number" || apartments.length == 0) {
+        throw new Error("Invalid input!");
+      }
+      apartments.map((apartment) => {
+        if (apartment >= minimalSquareMeters) {
+          resultArr.push(apartment);
+        }
+      });
+      return resultArr.join(', ');
+    },
+    isItAffordable(price, budget) {
+      if (typeof price !== "number" || typeof budget !== "number"
+       || price <= 0 || budget <= 0) {
+        throw new Error("Invalid input!");
+      }
+      let result = budget - price;
+      if (result < 0) {
+        return "You don't have enough money for this house!";
+      } else {
+        return "You can afford this home!";
+      }
+    },
+  };
+  
+  describe('findNewApartment', function() {
+    describe('isGoodLocation',function(){
+        it('error couse of invalid input',function(){
+            assert.throw(()=> findNewApartment.isGoodLocation(1, true), "Invalid input")
+            assert.throw(()=> findNewApartment.isGoodLocation('dsd', '100'), "Invalid input")
+            assert.throw(()=> findNewApartment.isGoodLocation(1, '100'), "Invalid input")
+        })
 
-    #createElement(parent,imageSource) {
-        let div = document.createElement("div");
-        let image = document.createElement("img");
-        image.src = imageSource;
-        let button = document.createElement("button");
-        button.textContent = "Hide image";
+        it('chech the location',function(){
+            assert.equal(findNewApartment.isGoodLocation('pernik',true), "This location is not suitable for you.")
+            assert.equal(findNewApartment.isGoodLocation('Sofia',true), "You can go on home tour!")
+            assert.equal(findNewApartment.isGoodLocation('Sofia',false), "There is no public transport in area.")
+            assert.equal(findNewApartment.isGoodLocation('Plovdiv',true), "You can go on home tour!")
+            assert.equal(findNewApartment.isGoodLocation('Plovdiv',false), "There is no public transport in area.")
+            assert.equal(findNewApartment.isGoodLocation('Varna',true), "You can go on home tour!")
+            assert.equal(findNewApartment.isGoodLocation('Varna',false), "There is no public transport in area.")
+        })
+    })
 
-        button.addEventListener("click",this.showHideImages.bind(this) )
+    describe('isLargeEnough',function(){
+       it('invalid parameters => error',function(){
+        assert.throw(()=> findNewApartment.isLargeEnough(1, '100'), "Invalid input")
+        assert.throw(()=> findNewApartment.isLargeEnough([], '100'), "Invalid input")
+        assert.throw(()=> findNewApartment.isLargeEnough(['sds'], '100'), "Invalid input")
+        assert.throw(()=> findNewApartment.isLargeEnough(1, 1), "Invalid input")
+       })
 
-        div.appendChild(image);
-        div.appendChild(button);
+       it('corect parameters => NOT error',function(){
+        assert.equal(findNewApartment.isLargeEnough([10,50,40],40), '50, 40')
+        assert.equal(findNewApartment.isLargeEnough([10,30,39],40), '')
+        assert.equal(findNewApartment.isLargeEnough([120,302,41],40), '120, 302, 41')
+       })
+    })
 
-        parent.appendChild(div);
-
-        return div;
-    }
-
-}
-let main = document.getElementById('main')
-let catImage = new InafeBox(main, 'https://http.cat/100')
+    describe('isItAffordable',function(){
+        it('invalid parameters => error AGAIN',function(){
+         assert.throw(()=> findNewApartment.isItAffordable('a', '100'), "Invalid input")
+         assert.throw(()=> findNewApartment.isItAffordable(-1, '100'), "Invalid input")
+         assert.throw(()=> findNewApartment.isItAffordable(1 ,-100), "Invalid input")
+         assert.throw(()=> findNewApartment.isLargeEnough(-1, -1), "Invalid input")
+        })
+ 
+        it('corect parameters => NOT error AGAIN',function(){
+         assert.equal(findNewApartment.isItAffordable(40,20), "You don't have enough money for this house!")
+         assert.equal(findNewApartment.isItAffordable(10,30),  "You can afford this home!")
+         assert.equal(findNewApartment.isItAffordable(10,10), "You can afford this home!")
+        })
+     })
+    
+  })
