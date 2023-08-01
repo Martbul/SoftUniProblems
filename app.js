2bb2624437f1d5fdf1cd5f34bb57948ff3da966c
Martbul <99181339+Martbul@users.noreply.github.com>
2023-08-01 23:09:09 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
+import page from '../node_modules/page/page.mjs';
+import {html ,render} from '../node_modules/lit-html/lit-html.js';
 import { logout } from './api/auth.js';
-import {html, render} from '../node_modules/lit-html/lit-html.js'
-import page from '../node_modules/page/page.mjs'
-import { showHome } from './views/home.js';
 import { getUserData } from './utils.js';
-import { showDashboard } from './views/dashboard.js';
+import { showHome } from './views/home.js';
 import { showLogin } from './views/login.js';
 import { showRegister } from './views/register.js';
+import { showDashboard } from './views/dashboard.js';
 import { showDetails } from './views/details.js';
 import { showEdit } from './views/edit.js';
 import { showCreate } from './views/create.js';
 
 
-
 const navTemplate = (user) =>html`
-       <section class="logo">
-                <img src="./images/logo.png" alt="logo">
-            </section>
-            <ul>
-                <!--Users and Guest-->
-                <li><a href="/">Home</a></li>
-                <li><a href="/dashboard">Dashboard</a></li>
-                ${!user 
-                  
-                  ?html` <!--Only Guest-->
-                <li><a href="/login">Login</a></li>
-                <li><a href="/register">Register</a></li>`
+  
+<div>
+            <a href="/dashboard">Fun Facts</a>
+          </div>
+            ${user 
                 
-                :html` <!--Only Users-->
-                <li><a href="/create">Create Postcard</a></li>
-                <li><a href="" @click=${onLogout}>Logout</a></li>`}
-            </ul>
-         
+                ?html`<!-- Logged-in users -->
+          <div class="user">
+            <a href="/create">Add Fact</a>
+            <a href="#" @click=${onLogout}>Logout</a>
+          </div>`
+          
+          :html` <!-- Guest users -->
+          <div class="guest">
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>`}
+          
 
+         
+          </div>
 `
 
 function updateNav(ctx, next) {
@@ -72,9 +72,9 @@ page(updateNav)
 page(decorateContext)
 
 page('/', showHome)
-page('/dashboard', showDashboard)
 page('/login', showLogin)
 page('/register', showRegister)
+page('/dashboard', showDashboard)
 page('/details/:id', showDetails)
 page('/edit/:id', showEdit)
 page('/create', showCreate)
